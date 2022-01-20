89a9a949f76972bfab598d0569ae08719c996e7c
Alina Kasianova <avkasyanova@gmail.com>
2022-01-20 14:24:30 +0200

    gallery styles fix

--- src/js/theme-switcher.js
+++ src/js/theme-switcher.js
@@ -1,4 +1,3 @@
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
@@ -27,8 +26,8 @@ function fixTheme() {
   const saveKey = localStorage.getItem('theme');
   if (!saveKey) {
     bodyRef.classList.add(Theme.DARK);
-      localStorage.setItem('theme', bodyRef.classList);
-      btnChange.checked = false;
+    localStorage.setItem('theme', bodyRef.classList);
+    btnChange.checked = false;
   } else {
     bodyRef.classList.add(saveKey);
     if (saveKey === Theme.LIGHT) {
@@ -36,65 +35,3 @@ function fixTheme() {
     }
   }
 }
-
-// ПРЕЖНИЙ КОД
-
-// const Theme = {
-//   LIGHT: 'light-theme',
-//   DARK: 'dark-theme',
-// };
-
-// const btnChange = document.querySelector('#theme-switch-toggle');
-// btnChange.addEventListener('change', themeChange);
-
-
-// const bodyRef = document.body;
-// const containerRef = document.querySelector('.container');
-// const headerRef = document.querySelector('.header-bg');
-// const footerRef = document.querySelector('.footer-section');
-
-// const fixTheme = localStorage.getItem('theme');
-
-// if (!fixTheme) {
-//     localStorage.setItem('theme', Theme.DARK)
-//     bodyRef.classList.add('dark-theme');
-// }
-
-// function themeChange() {
-//     if (btnChange.checked !== true) {
-//         bodyRef.classList.add('dark-theme');
-//         bodyRef.classList.remove('light-theme');
-//         headerRef.classList.add('dark-theme');
-//         headerRef.classList.remove('light-theme');
-//         footerRef.classList.add('dark-theme');
-//         footerRef.classList.remove('light-theme');
-//         localStorage.setItem('theme', Theme.DARK);
-//     } else {
-//         bodyRef.classList.add('light-theme');
-//         bodyRef.classList.remove('dark-theme');
-//         headerRef.classList.add('light-theme');
-//         headerRef.classList.remove('dark-theme');
-//         footerRef.classList.add('light-theme');
-//         footerRef.classList.remove('dark-theme');
-//         localStorage.setItem('theme', Theme.LIGHT);
-//     }
-// }
-
-// if (fixTheme === Theme.LIGHT) {
-//     bodyRef.classList.add('light-theme');
-//     bodyRef.classList.remove('dark-theme');
-//     headerRef.classList.add('light-theme');
-//     headerRef.classList.remove('dark-theme');
-//     footerRef.classList.add('light-theme');
-//         footerRef.classList.remove('dark-theme');
-//      btnChange.checked = true;
-// } else if (fixTheme === Theme.DARK) {
-//     bodyRef.classList.add('dark-theme');
-//     bodyRef.classList.remove('light-theme');
-//     headerRef.classList.add('dark-theme');
-//     headerRef.classList.remove('light-theme');
-//     footerRef.classList.add('dark-theme');
-//         footerRef.classList.remove('light-theme');
-
-// }
-
